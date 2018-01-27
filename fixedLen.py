e87c925a787e245bea1739a8e5e2949715666a6b
servo270 <32885969+servo270@users.noreply.github.com>
2018-01-27 14:50:59 -0500

    documentation
    Changed functionality to be more general

--- fixedLen.py
+++ fixedLen.py
@@ -1,14 +1,21 @@
-import os #handy library for interacting with files
-import random
+#simple program for opening a text file and adding whitespace such that 
+#each line has the same length
+#useful for parsing data and seeking to a specific line
+
+import os 
 print(os.getcwd()) #print out the file location
-os.chdir('files') #change the file location to the 'files' folder
+os.chdir(input("directory: ") #change the file location to the target folder
 print(os.getcwd()) #print out the new file location
 
 fil = input("File name: ")
-raw = open(fil + '.txt','r')
-file = [n.strip() for n in raw.readlines()]
-maxLen = max([len(n) for n in file])
+raw = open(fil + '.txt','r') #open the file
+         
+file = [n.strip() for n in raw.readlines()] #strip off tabs from the front and back
+maxLen = max([len(n) for n in file]) #determine the maximum length of lines
 print(maxLen)
-output = open(fil +'Rect.txt','w')
-for i in file: output.write("%-*s \n" % (maxLen,i))
-output.close()
+
+output = open(fil +'Rect.txt','w') #create a new file for the edited data
+for i in file: output.write("%-*s \n" % (maxLen,i)) #write the file
+
+output.close() #close the opened files
+raw.close()
