4fe61d72c8599461b9aab5e55844f84b8831696a
servo270 <32885969+servo270@users.noreply.github.com>
2018-01-27 14:43:31 -0500

    Documentation

--- Newton/springCollar.py
+++ Newton/springCollar.py
@@ -1,19 +1,21 @@
-#spring model
+#model systems acting under gravity, friction and springs
+#used to check physics homework problems
+
 from math import sin, cos, radians
 
-massOne = 10
-timeStep = .1
+massOne = 10 #kg
+timeStep = .1 #s
 step = 0
-sprPos = .75
-sprConst = 1000
+sprPos = .75 #m
+sprConst = 1000 #N/m
 angle = 50 #angle off horizontal
-fricCoeff = .28
+fricCoeff = .28 
 
 constants = (massOne,timeStep,fricCoeff,angle,sprConst)
 
-posOne = 15
-velOne = -6
-accOne = 0
+posOne = 15 #m
+velOne = -6 #/ms
+accOne = 0 #m/s/s
 disp = posOne + sprPos
 
 variables = [velOne,posOne,accOne,disp]
@@ -43,16 +45,18 @@ def getPosition(constants,variables):
     variables[3] = variables[1] + sprPos                #recaclulate the distance to the spring
     return variables
 
-#posData = []
-while step < 100:
-#while variables[1] > 0:
-#while variables[0] <= -0.1:
-    print(int((variables[3] + 10)) * ' ' , '*')
+#posData = [] #used to graph position
+while step < 100: 
+#while variables[1] > 0: #stop when it reaches the ground
+#while variables[0] <= -0.1: #stop when it comes to rest
+    print(int((variables[3] + 10)) * ' ' , '*') #plots position vs time in the command line
     variables = getAccel(constants,variables)
     variables = getVelocity(constants,variables)
     variables = getPosition(constants,variables)
     step += 1
-    #posData.append(variables[1])
+    #posData.append(variables[1]) #used to graph position
+    
+'''print the final condition'''
 print(step*timeStep, ' Seconds')
 print(variables)
 print(posData)
