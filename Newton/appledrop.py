92215b776a12588e33c3c85dc2ee1fad8352615a
servo270 <32885969+servo270@users.noreply.github.com>
2018-01-27 14:34:48 -0500

    Update appledrop.py
    commented code better

--- Newton/appledrop.py
+++ Newton/appledrop.py
@@ -1,5 +1,10 @@
-#spring model
+#Stepwise physics simulator
+#gravity, friction, springs, drag, and restitution
+
+
 from math import sin, cos, radians, pi
+
+'''set up some initial conditions'''
 #apple: .136 tennis ball: .0585
 massOne = 130 / 1.6
 timeStep = .001
@@ -10,13 +15,12 @@ angle = 90 #angle off horizontal
 fricCoeff = 0
 area = .6
 dragCoeff = .8
-airDens = 1.2
+airDens = 1.3
 resConst = .1
 
 constants = (massOne,timeStep,fricCoeff,angle,sprConst, area, dragCoeff, airDens)
 
 term = 0
-#posOne = (6*10.66 + 14 + 3.5)/ 3.1
 posOne = 11.4
 velOne = -.1
 accOne = 0
@@ -32,11 +36,11 @@ def getAccel(constants,variables):
     else: forceS = 0
     
     forceF = - fricCoeff * massOne * 9.81 * cos(radians(angle)) #force of friction
-    if variables[0] <= 0:
+    if variables[0] <= 0: #friction opposes the direction of motion
         forceF *= -1
-    #forceD = 0
+    
     forceD = -.5 * dragCoeff * airDens * area * variables[0] * abs(variables[0])
-    #print(forceF,forceG,forceS,forceD)
+    
     variables[2] = (forceG + forceF + forceS + forceD) / massOne #sum of forces= ma
     return variables
 
@@ -51,13 +55,12 @@ def getPosition(constants,variables):
     return variables
 
 
-count = 0
-flag = 0
-#while step < 100:
-while variables[1] > -45:
-#while not (.1 > abs(variables[0]) and count >= 20):
-#while not variables[4]:
-    #print(int((variables[3] + 1)*30) * ' ' , '*')
+count = 0 #count the number of bounces
+flag = 0 #used to determine when to print the bounce text
+#while step < 100: #for a certain number of steps
+while variables[1] > -45: #
+#while not (.1 > abs(variables[0]) and count >= 20): #while it is moving
+    #print(int((variables[3] + 1)*30) * ' ' , '*') #use this line to plot in the command line. Not recommended for low steps
     variables = getAccel(constants,variables)
     variables = getVelocity(constants,variables)
     if variables[1] < 0 and variables[0] < 0:
@@ -66,19 +69,21 @@ while variables[1] > -45:
         print('boing!', variables[0])
         
     variables = getPosition(constants,variables)
-    if abs(variables[0]) < .01:
+    if abs(variables[0]) < .01: #check if it has bounced
         flag = (1 - flag)
         count += .5
         print(str(count) * flag ,str(variables[1]) * flag)
-        if variables[1] < .05:
+        if variables[1] < .05: #if it isn't bouncing a significant amount, break
             break
     step += 1
-if resConst != 0:
+if resConst != 0: #get better information about the end condition
     variables = getAccel(constants,variables)
     variables = getVelocity(constants,variables)
     if variables[1] < 0:
         variables[0] = - (resConst * variables[0])
     variables = getPosition(constants,variables)
     step += 1
+    
+#display the end condition
 print(step*timeStep, ' Seconds')
 print(variables)
