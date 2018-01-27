ff04e77657a3e1cb2c1b02e210197c4d54b350b9
servo270 <32885969+servo270@users.noreply.github.com>
2018-01-27 14:40:36 -0500

    Documented code

--- Newton/PhysicsMajors.py
+++ Newton/PhysicsMajors.py
@@ -1,17 +1,21 @@
+#program for solving problems involving how long it will take two objects to
+#contact each other under their own gravity
 
-massOne = 165
+#uses a stepped approxamation
+
+massOne = 165 #kg
 massTwo = 125
 gravParam = 6.67408*(10**(-11))
-timeStep = 100003
+timeStep = 100 #seconds
 step = 0
 
 constants = (massOne,massTwo,gravParam,timeStep)
 
-velOne = 0
+velOne = 0 #m/s
 velTwo = 0
-posOne = 0
-posTwo = 15
-accOne = 0
+posOne = 0 #m
+posTwo = 15 
+accOne = 0 #m/s/s
 accTwo = 0
 dist = posTwo-posOne
 
@@ -32,7 +36,6 @@ def getVelocity(constants,variables):
 #    print(variables[1])
     return variables
 
-
 def getPosition(constants,variables):
     variables[2] += variables[0]*timeStep
     variables[3] += variables[1]*timeStep
@@ -47,6 +50,8 @@ while variables[6] > 0:
     variables = getVelocity(constants,variables)
     variables = getPosition(constants,variables)
     step += 1
+    
+'''print the end condition in a number of different units'''
 print(step*timeStep)
 print(step*timeStep/3600, " hours")
 print(step*timeStep/3600/24, " days")
